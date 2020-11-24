1a83c43adcfe92d79276c6f712d620a10a64e431
Yaroslav Klymko <t3hnar@gmail.com>
2020-11-24 22:54:00 +0100

    add Derive.scala

---
+++ src/main/scala/com/evolution/Derive.scala
@@ -0,0 +1,35 @@
+package com.evolution
+
+import cats.Apply
+import cats.syntax.all._
+
+trait Derive[A, B, C] {
+
+  def apply(a: A, b: B): C
+}
+
+object Derive extends DeriveImplicits0 {
+
+  def apply[A, B, C](implicit derive: Derive[A, B, C]): Derive[A, B, C] = derive
+
+  implicit def productRightDerive[F[_]: Apply, A]: Derive[F[Unit], F[A], F[A]] = (a, b) => a.productR(b)
+
+  implicit def rightDerive[A]: Derive[Unit, A, A] = (_, a) => a
+}
+
+sealed abstract class DeriveImplicits0 extends DeriveImplicits1 {
+
+  implicit def leftDerive[A]: Derive[A, Unit, A] = (a, _) => a
+
+  implicit def productLeftDerive[F[_]: Apply, A]: Derive[F[A], F[Unit], F[A]] = (a, b) => a.productL(b)
+}
+
+sealed abstract class DeriveImplicits1 extends DeriveImplicits2 {
+
+  implicit def productDerive[F[_]: Apply, A, B]: Derive[F[A], F[B], F[(A, B)]] = (a, b) => a.product(b)
+}
+
+sealed abstract class DeriveImplicits2 {
+
+  implicit def bothDerive[A, B]: Derive[A, B, (A, B)] = (a, b) => (a, b)
+}
---
+++ src/test/scala/com/evolution/DeriveTest.scala
@@ -0,0 +1,58 @@
+package com.evolution
+
+import cats.syntax.all._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+
+class DeriveTest extends AnyFunSuite with Matchers {
+  import DeriveTest._
+
+  test("derive both") {
+    val expected = ("a", "b")
+    val actual: (String, String) = derive("a", "b")()
+    actual shouldEqual expected
+  }
+
+  test("derive left") {
+    val actual: String = derive("", ())()
+    actual shouldEqual ""
+  }
+
+  test("derive right") {
+    val actual: String = derive((), "")()
+    actual shouldEqual ""
+  }
+
+  test("derive unit") {
+    val expected = ()
+    val actual: () = derive((), ())()
+    actual shouldEqual expected
+  }
+
+  test("derive product") {
+    val actual: Option[(String, String)] = derive("a".some, "b".some)()
+    actual shouldEqual ("a", "b").some
+  }
+
+  test("derive product left") {
+    val actual: Option[String] = derive("".some, ().some)()
+    actual shouldEqual "".some
+  }
+
+  test("derive product right") {
+    val actual: Option[String] = derive(().some, "".some)()
+    actual shouldEqual "".some
+  }
+}
+
+object DeriveTest {
+
+  def derive[A, B](a: A, b: B): Apply[A, B] = new Apply[A, B] {
+    def apply[C]()(implicit derive: Derive[A, B, C]): C = derive(a, b)
+  }
+
+  trait Apply[A, B] {
+    def apply[C]()(implicit derive: Derive[A, B, C]): C
+  }
+}
