40f2c214ab184fc2a757415bef2ee0e89d84d7b5
Yaroslav Klymko <t3hnar@gmail.com>
2021-03-23 11:11:52 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val `kind-projector` = "org.typelevel"  % "kind-projector" % "0.11.0"
 
   object Cats {
-    private val version = "2.2.0"
+    private val version = "2.3.0"
     val core      = "org.typelevel" %% "cats-core"      % version
     val alleycats = "org.typelevel" %% "alleycats-core" % version
     val effect    = "org.typelevel" %% "cats-effect"    % version
