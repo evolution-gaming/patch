12c5c5dfcf47132d8049cfa5118fc326b36856c4
sshevchenko <sshevchenko@evolution.com>
2026-07-18 00:36:24 +0200

    Bump scalatest, discipline-scalatest, cats-effect, cats

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.19"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.20"
 
   val `kind-projector`       = "org.typelevel"  % "kind-projector"       % "0.13.4"
-  val `discipline-scalatest` = "org.typelevel" %% "discipline-scalatest" % "2.2.0"
-  val `cats-effect`          = "org.typelevel" %% "cats-effect"          % "3.4.8"
+  val `discipline-scalatest` = "org.typelevel" %% "discipline-scalatest" % "2.3.0"
+  val `cats-effect`          = "org.typelevel" %% "cats-effect"          % "3.7.0"
 
   object Cats {
-    private val version = "2.9.0"
+    private val version = "2.13.0"
     val core = "org.typelevel" %% "cats-core" % version
     val laws = "org.typelevel" %% "cats-laws" % version
   }
